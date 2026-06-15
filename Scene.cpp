d6c8d6d29c59d816e3bf6681a7f3cbec83c13666
c-dvoid <dadashovsam5@gmail.com>
2026-06-15 15:12:53 +0400

    Add Scene class

---
+++ Scene.cpp
@@ -0,0 +1,37 @@
+
+#include "Scene.h"
+
+#include <iostream>
+
+Scene::Scene() {
+    ambientStrength = 0.6f;
+    spawnPoint = glm::vec3(0.0f);
+    light.position = glm::vec3(5.0f, 10.0f, 5.0f);
+    light.color = glm::vec3(1.0f);
+}
+
+Scene::~Scene() {
+    for (auto& obj : objects) {
+        delete obj.mesh;
+    }
+}
+
+void Scene::update(glm::vec3 playerPos,
+    std::function<void(std::string, std::string)> onTrigger) {
+    for (auto& trigger : triggers) {
+        if (trigger.activated) continue;
+
+        float dist = glm::distance(playerPos, trigger.position);
+        if (dist < trigger.radius) {
+            trigger.activated = true;
+            onTrigger(trigger.action, trigger.parameter);
+        }
+    }
+}
+
+void Scene::draw(Shader& shader) {
+    for (auto& obj : objects) {
+        shader.setMat4("model", obj.transform);
+        obj.mesh->draw(shader);
+    }
+}
---
+++ Scene.h
@@ -0,0 +1,46 @@
+
+#pragma once
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+
+#include <string>
+#include <vector>
+#include <functional>
+
+#include "Mesh.h"
+#include "Shader.h"
+
+struct SceneObject {
+    Mesh* mesh;
+    glm::mat4 transform;
+};
+
+struct Trigger {
+    glm::vec3 position;
+    float radius;
+    bool activated;
+    std::string action;
+    std::string parameter;
+};
+
+struct SceneLight {
+    glm::vec3 position;
+    glm::vec3 color;
+};
+
+class Scene {
+public:
+    std::vector<SceneObject> objects;
+    std::vector<Trigger>     triggers;
+    SceneLight               light;
+    glm::vec3                spawnPoint;
+    std::string              nextLevel;
+    float                    ambientStrength;
+
+    Scene();
+    ~Scene();
+
+    void update(glm::vec3 playerPos, std::function<void(std::string, std::string)> onTrigger);
+    void draw(Shader& shader);
+};
